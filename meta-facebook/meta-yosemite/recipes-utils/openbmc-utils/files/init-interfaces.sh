d20e3d432401663ec388e83e1c7451ac9be8853f
Brandon Gillespie <bjg@solv.com>
2024-04-19 12:34:36 -0600

    fixing

--- meta-facebook/meta-yosemite/recipes-utils/openbmc-utils/files/init-interfaces.sh
+++ meta-facebook/meta-yosemite/recipes-utils/openbmc-utils/files/init-interfaces.sh
@@ -8,8 +8,13 @@
 # Default-Stop:
 # Short-Description: Symlink for interfaces
 ### END INIT INFO
+
+networkdir=/etc/network
+interfaces=$networkdir/interfaces
+persistent=/mnt/data
+
 create_interfaces() {
-cat > /mnt/data/etc/interfaces<< EOF
+  cat > $persistent$interfaces<< EOF
 auto lo
 iface lo inet loopback
 
@@ -19,46 +24,33 @@ iface eth0 inet manual
   up ip link set eth0 up
   up dhclient -4 -d -pf /var/run/dhclient.eth0.pid eth0 > /dev/null 2>&1 &
 EOF
-
-#append_vlan
 }
 
-append_vlan() {
-cat >> /mnt/data/etc/interfaces<< EOF
-auto vlan42
-iface vlan42 inet static
-  vlan-raw-device eth0
-  address   10.1.2.3
-  netmask   255.255.255.0
-EOF
+start() {
+   if [ ! -L $interfaces ];then
+        if [ ! -f $persistent$interfaces ];then
+            mkdir -p $networkdir
+            create_interfaces
+        fi
+        ln -sf $persistent$interfaces $interfaces
+   fi
 }
 
+################################################################################
 # Source function library.
 . /etc/init.d/functions
 
-start() {
-    if [ ! -L /etc/network/interfaces ];then
-        if [ ! -f /mnt/data/etc/network/interfaces ];then
-            mkdir -p /mnt/data/etc/network
-            create_interfaces
-        fi
-        ln -sf /mnt/data/etc/network/interfaces /etc/network/interfaces
-    fi
-    modprobe 8021q
-}
-stop() {
-    #Don't need to do anything on stop
-    echo "Stopping init-interfaces.sh"
-}
-case "$1" in 
+set -euo pipefail
+#set +x
+
+case "$1" in
     start)
+    echo "start"
        start
        ;;
     stop)
-       stop
        ;;
     restart)
-       stop
        start
        ;;
     status)
@@ -72,4 +64,4 @@ case "$1" in
        echo "Usage: $0 {start|stop|status|restart}"
 esac
 
-exit 0 
+exit 0
